e06a7bdb604d930dca0514e28cdfe173d3fff9aa
Malsaeed276 <mohamed.elsaeed276@gmail.com>
2021-09-30 16:47:24 +0300

    adding todo

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
+      //TODO the object counter should update too
+      //you can use GetX state management
       _counter++;
     });
   }
@@ -52,9 +54,11 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
+              //TODO Show the Selected object title
               'object Title:',
             ),
             Text(
+              //TODO Show the Selected object counter and the goal
               '$_counter / goal',
               style: Theme.of(context).textTheme.headline4,
             ),
